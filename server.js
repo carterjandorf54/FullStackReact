85ffd3859be77da8032f5c89b2c6958413a191dd
Carter Jandorf <carter@cjandorf.com>
2022-11-13 22:43:50 -0800

    Established Connection to MONGO and Created User Module

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 require("dotenv").config();
-const e = require("express");
 const express = require("express");
+const mongoose = require("mongoose");
+
+// import routes
+const authRoute = require("./routes/auth");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.post("/name", (req, res) => {
-  if (req.body.name) {
-    return res.json({ name: req.body.name });
-  } else {
-    return res.status(400).json({ error: "No name provided" });
-  }
-});
-
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("Vidly Express Server");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+app.use("/api/auth", authRoute);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to Database");
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
