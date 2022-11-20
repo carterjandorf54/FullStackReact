9474336a7f50990a308b229410c3d6eedb803b10
Carter Jandorf <carter@cjandorf.com>
2022-11-20 13:42:58 -0800

    Added NewToDo to the Dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useGlobalContext } from "../context/GlobalContext";
 import { useNavigate } from "react-router-dom";
 import ToDoCard from "./ToDoCard";
+import NewToDo from "./NewToDo";
 
 const Dashboard = () => {
   const { user, completeToDos, incompleteToDos } = useGlobalContext();
@@ -15,8 +16,8 @@ const Dashboard = () => {
 
   return (
     <div className="dashboard">
+      <NewToDo />
       <div className="todos">
-        <h2 className="todos__title">Incomplete ToDo's</h2>
         {incompleteToDos.map((toDo) => (
           <ToDoCard toDo={toDo} key={toDo._id} />
         ))}
