cf2a6fb18d65e37b0dd7a66b8aaf18bfbb67ebcc
Carter Jandorf <carter@cjandorf.com>
2022-11-20 21:31:27 -0800

    Fixed Bug for Deployment

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ const path = require("path");
 const authRoute = require("./routes/auth");
 const toDosRoute = require("./routes/todos");
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
-
 const app = express();
 
 app.use(express.json());
@@ -26,6 +20,12 @@ app.get("/api", (req, res) => {
 
 app.use("/api/auth", authRoute);
 app.use("/api/todos", toDosRoute);
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 mongoose
   .connect(process.env.MONGO_URI)
